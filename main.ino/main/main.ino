55225ca562423cec2f20b4faac327d0c178486ee
PartoTheEagle <asalhaytham@gmail.com>
2017-07-15 18:18:39 +0100

    Feature/communication checker (#4)
    * add comment to the main module
    
    * linting
    
    * convert taskTwo into a communication module bluePrint
    
    * using the comm module and running task testCan to test the can bus communication every 10 ticks
    
    * add initCan method to initialize the can bus
    
    * add message to receive, message to send external variable and back controller Id
    
    * implement Receive message, send message and partial implementation of testCan
    
    * init can and run testCan task in main module
    
    * deliverMessage is not running as a separate task
    
    * refactor deliverMessage method
    
    * add vars to test the communication down time
    
    * code linting
    
    * now pin 2 will go high if there is can fault
    
    * update messageCount when can fail to initialize
    
    * no need to check the message count when sending

--- main.ino/main/main.ino
+++ main.ino/main/main.ino
@@ -1,12 +1,14 @@
+/**
+ * This is the main module in the back controller program
+ * The main program is running on Arduino Mega 2560
+ */
 #include <Arduino_FreeRTOS.h>
 #include "motorController.h"
-#include "taskTwo.h"
+#include "comm.h"
 
-void TaskAnalogRead( void *pvParameters );
 // the setup function runs once when you press reset or power the board
 void setup() {
-
-  // Now set up two tasks to run independently.
+    initCan();
   xTaskCreate(
     controlMotor
     ,  (const portCHAR *)"Blink"   // A name just for humans
@@ -16,17 +18,18 @@ void setup() {
     ,  NULL );
 
   xTaskCreate(
-    blink
-    ,  (const portCHAR *)"Blink"   // A name just for humans
+     testCan
+    ,  (const portCHAR *)"TestCan"   // A name just for humans
     ,  128  // Stack size
     ,  NULL
     ,  2  // priority
     ,  NULL );
+
 
   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
 
 void loop()
 {
-  // Empty. Things are done in Tasks.
+ 
 }
--- main.ino/main/taskTwo.h
+++
@@ -1,6 +0,0 @@
-#ifndef TaskTwo_h
-#define TaskTwo_h
-#include "Arduino.h"
-
-void blink( void *pvParameters );
-#endif
